a5273c36212e670655ff2d571f488ea580cb491a
AutomationTry <susan.help.test@gmail.com>
2023-07-10 00:33:41 +0300

    adding waits

--- src/test/java/com/demoqa/pageobjects/AbstractPageObject.java
+++ src/test/java/com/demoqa/pageobjects/AbstractPageObject.java
@@ -1,6 +1,7 @@
 package com.demoqa.pageobjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -38,4 +39,27 @@ public class AbstractPageObject {
     public void waitForInvisibility(By by, int DURATION_TO_WAIT) {
         new WebDriverWait(driver, Duration.ofSeconds(DURATION_TO_WAIT)).until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
+
+//    /**
+//     * Wait for specific ExpectedCondition for the given amount of time in seconds
+//     */
+//    public void waitFor(ExpectedCondition<WebElement> condition, Duration timeOut) {
+//        timeOut = timeOut != null ? timeOut : Duration.ofSeconds(30);
+//        WebDriverWait wait = new WebDriverWait(driver, timeOut);
+//        wait.until(condition);
+//    }
+//
+//    protected void waitForVisibilityOfWebElement(By WebElement, Duration... timeOut) {
+//        int attempts = 0;
+//        while (attempts < 2) {
+//            try {
+//                waitFor(ExpectedConditions.visibilityOfElementLocated(WebElement),
+//                        (timeOut.length > 0 ? timeOut[0] : null));
+//                break;
+//            } catch (StaleElementReferenceException e) {
+//            }
+//            attempts++;
+//        }
+//
+//    }
 }
